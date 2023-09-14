1366eb09e860fb56ea3bcdc4875af95310cd70c0
Yoha-Potel <cpoteljunot@hotmail.com>
2023-09-14 12:21:17 -0300

    se modifico archivos a modo de prueba

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('home', { 
+    mensaje: 'Probando ' 
+  });
+});
+
+
 module.exports = router;
